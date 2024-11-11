a8ba55790a1e6b422dc911560b6e971785da6b30
melbulaf <mebulaf@unal.edu.co>
2024-11-11 17:31:40 -0500

    Ejercicio 3
    Prueba 1

---
+++ main.py
@@ -0,0 +1,53 @@
+
+
+class Asiento:
+    def __init__ (self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self,cambio):
+        if cambio == "blanco" or cambio == "negro" or cambio == "amarillo" or cambio == "verde" or cambio == "rojo":
+            self.color = cambio
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self,num):
+        self.registro = num
+
+    def asignarTipo(self,Tipo):
+        if Tipo == "electrico" or Tipo == "gasolina":
+            self.tipo = Tipo
+
+class Carro:
+    cantidadCreados = 0
+    def __init__ (self, modelo, precio, asientos, marca, motor, registro,cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Carro.cantidadCreados = cantidadCreados
+    
+    def cantidadAsientos(self):
+        contador = 0
+        for n in range(0, len(self.asientos), 1):
+            if self.asientos[n] != None:
+                contador = contador + 1
+        return contador
+    
+    def verificarIntegridad(self):
+        if self.registro == self.motor.registro:
+            for l in range (0, len(self.asientos),1):
+                if self.asientos[l] != None:
+                    if self.asientos[l].registro != self.registro:
+                        return "Las piezas no son originales"
+                else:
+                    return "Auto original"
+        else:
+            return "Las piezas no son originales"
