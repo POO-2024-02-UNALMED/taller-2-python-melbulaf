3dc121038e93890db2379c21eadefe4b352385f8
melbulaf <mebulaf@unal.edu.co>
2024-11-11 17:49:08 -0500

    Ejercicio 3
    Prueba 4

--- main.py
+++ main.py
@@ -24,14 +24,13 @@ class Motor:
 
 class Auto:
     cantidadCreados = 0
-    def __init__ (self, modelo, precio, asientos, marca, motor, registro,cantidadCreados):
+    def __init__ (self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        Carro.cantidadCreados = cantidadCreados
     
     def cantidadAsientos(self):
         contador = 0
